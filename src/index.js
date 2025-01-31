94623fc70b5ade0cb89efb988aedb5bf63bbe874
yaswanthp7799 <yaswanthp7799@gmail.com>
2025-02-01 00:43:48 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>
+  <React.StrictMode>    
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
